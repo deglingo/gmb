1d9aa7a233370d43cce8a3805796e80c5db7dc86
deglingo <alfredeneumeuh AT laposte DOT net>
2014-07-15 11:19:20 +0200

    added some client and server

--- gmb/gmbapp.py
+++ gmb/gmbapp.py
@@ -1,5 +1,7 @@
 #
 
+import socket, time
+
 
 # GmbApp:
 #
@@ -18,6 +20,24 @@ class GmbApp :
     #
     def run (self) :
         print('gmb: hello')
+        host = 'localhost'
+        port = 5555
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ntries = 0
+        while True :
+            try:
+                s.connect((host, port))
+            except ConnectionRefusedError:
+                if ntries > 10 :
+                    raise
+                else :
+                    print('connection refused, retry...')
+                    time.sleep(0.1)
+            break
+        s.sendall(b'Hello, world')
+        data = s.recv(1024)
+        s.close()
+        print('Received', repr(data))
 
 
 # exec
--- gmb/gmbdapp.py
+++ gmb/gmbdapp.py
@@ -1,5 +1,43 @@
 #
 
+import socket, signal, threading
+
+
+# Server
+#
+class Server :
+
+
+    # __init__:
+    #
+    def __init__ (self) :
+        self.host = ''
+        self.port = 5555
+
+        
+    # start:
+    #
+    def start (self) :
+        print('starting server...')
+        self.listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listen_sock.bind((self.host, self.port))
+        self.listen_sock.listen(1)
+        self.listen_thread = threading.Thread(target=self.__listen_T)
+        self.listen_thread.start()
+
+
+    # __listen_T:
+    #
+    def __listen_T (self) :
+        while True :
+            conn, addr = self.listen_sock.accept()
+            print('Connected by', addr)
+            # while True:
+            #     data = conn.recv(1024)
+            #     if not data: break
+            #     conn.sendall(data)
+            # conn.close() # [FIXME]
+
 
 # GmbdApp:
 #
@@ -18,6 +56,9 @@ class GmbdApp :
     #
     def run (self) :
         print('gmbd: hello')
+        self.server = Server()
+        self.server.start()
+        signal.pause()
 
 
 # exec
