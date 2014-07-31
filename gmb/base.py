cb28b53141219725f995e72ab6fd061a3a15998b
deglingo <alfredeneumeuh AT laposte DOT net>
2014-07-31 11:01:35 +0200

    added the LOG_LEVEL_* constants

--- gmb/base.py
+++ gmb/base.py
@@ -1,6 +1,13 @@
 # base.py - basic stuff for gmb and gmbd
 
 __all__ = [
+    'LOG_LEVEL_DEBUG',
+    'LOG_LEVEL_INFO',
+    'LOG_LEVEL_WARNING',
+    'LOG_LEVEL_ERROR',
+    'LOG_LEVEL_CRITICAL',
+    'LOG_LEVEL_CMDOUT',
+    'LOG_LEVEL_CMDERR',
     'log_setup',
     'log',
     'trace',
@@ -11,6 +18,29 @@ import sys, logging
     
 
 LOG_DOMAIN = None
+
+
+# Log levels
+#
+LOG_LEVEL_DEBUG    = logging.DEBUG
+LOG_LEVEL_INFO     = logging.INFO
+LOG_LEVEL_WARNING  = logging.WARNING
+LOG_LEVEL_ERROR    = logging.ERROR
+LOG_LEVEL_CRITICAL = logging.CRITICAL
+
+LOG_LEVEL_CMDOUT   = LOG_LEVEL_INFO + 1
+LOG_LEVEL_CMDERR   = LOG_LEVEL_INFO + 2
+
+LOG_LEVEL_ALL = set((LOG_LEVEL_DEBUG,
+                     LOG_LEVEL_INFO,
+                     LOG_LEVEL_WARNING,
+                     LOG_LEVEL_ERROR,
+                     LOG_LEVEL_CRITICAL,
+                     LOG_LEVEL_CMDOUT,
+                     LOG_LEVEL_CMDERR))
+
+# just in case, make sure all levels are unique
+assert len(LOG_LEVEL_ALL) == 7
 
 
 # log_setup:
