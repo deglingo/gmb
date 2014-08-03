250547b1adab91866b9664d6c972151e5244c477
deglingo <alfredeneumeuh AT laposte DOT net>
2014-08-03 18:37:15 +0200

    gmb: added the log domain filter

--- gmb/gmbapp.py
+++ gmb/gmbapp.py
@@ -17,6 +17,18 @@ OPTIONS:
 """
 
 
+# LogDomainFilter:
+#
+class LogDomainFilter :
+
+    def filter (self, rec) :
+        if rec.levelno in (LOG_LEVEL_CMDOUT, LOG_LEVEL_CMDERR) :
+            rec.dom_prefix = ''
+        else :
+            rec.dom_prefix = '%s: ' % rec.name
+        return True
+
+
 # GmbApp:
 #
 class GmbApp :
@@ -35,8 +47,10 @@ class GmbApp :
     def run (self) :
         self.logger = log_setup('gmb')
         hdlr = logging.StreamHandler(sys.stderr)
-        fmt = logging.Formatter('%(name)s: %(message)s')
+        fmt = logging.Formatter('%(dom_prefix)s%(message)s')
         hdlr.setFormatter(fmt)
+        dom_filt = LogDomainFilter()
+        hdlr.addFilter(dom_filt)
         filt = LogLevelFilter()
         hdlr.addFilter(filt)
         self.logger.addHandler(hdlr)
